ef6a8a06200f2b327aed9c556080de56e15a7509
jaymasterflex <rylerkorey@gmail.com>
2020-07-22 20:37:19 +0100

    redid my portfolio

--- main.js
+++ main.js
@@ -16,3 +16,18 @@ for (var i = 0; i < navItem.length; i++) {
   this.className += " active";
   });
 }
+
+
+
+window.onscroll = function() { myFunction() };
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
